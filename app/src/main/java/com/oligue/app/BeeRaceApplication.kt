f069d40f68e845ba68a39f73606f8cc4aef22d54
Victor Guedes <victoroligue@gmail.com>
2024-12-17 21:20:40 +0000

    add application

---
+++ app/src/main/java/com/oligue/app/BeeRaceApplication.kt
@@ -0,0 +1,20 @@
+package com.oligue.app
+
+import android.app.Application
+import com.oligue.app.beerace.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class BeeRaceApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@BeeRaceApplication)
+            modules(appModules)
+        }
+    }
+}
