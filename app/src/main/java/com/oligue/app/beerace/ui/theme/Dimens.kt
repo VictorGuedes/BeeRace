f6e97b10050beff6b93b9f30091fad03558ecded
Victor Guedes <victoroligue@gmail.com>
2024-12-18 21:09:50 +0000

    add dimens files

---
+++ app/src/main/java/com/oligue/app/beerace/ui/theme/Dimens.kt
@@ -0,0 +1,11 @@
+package com.oligue.app.beerace.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val dimen_2 = 2.dp
+val dimen_4 = 4.dp
+val dimen_8 = 8.dp
+val dimen_16 = 16.dp
+val dimen_64 = 64.dp
+val dimen_20sp = 20.sp
